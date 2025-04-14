fe21afefbc0aa155f43ae10fe9cc56504d5aa785
Robert Vegh <robert.g.vegh@gmail.com>
2025-04-14 16:22:25 +0200

    datepicker for adding due dates, some css fixes

--- frontend/src/todos/components/TodoListContent.jsx
+++ frontend/src/todos/components/TodoListContent.jsx
@@ -7,12 +7,17 @@ import {
   Button,
   Typography,
   Checkbox,
+  Chip,
 } from '@mui/material'
 import DeleteIcon from '@mui/icons-material/Delete'
 import AddIcon from '@mui/icons-material/Add'
 import CheckCircleIcon from '@mui/icons-material/CheckCircle'
 import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked'
 import { updateTodoList, getTodoListById } from '../../lib/actions'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { DatePicker } from '@mui/x-date-pickers/DatePicker'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import dayjs from 'dayjs'
 
 export const TodoListContent = ({ activeList, setListCompletion }) => {
   const [todos, setTodos] = useState([])
@@ -42,6 +47,24 @@ export const TodoListContent = ({ activeList, setListCompletion }) => {
     return todos.every((todo) => todo.completed)
   }
 
+  const getDueDateDifference = (dueDate) => {
+    if (!dueDate) return null
+    const dueDateDayjs = dayjs(dueDate)
+    const dayDifference = dueDateDayjs.diff(dayjs(), 'day')
+    if (dayDifference === 0) return { label: 'Today', color: 'warning', variant: 'filled' }
+    else if (dayDifference === 1)
+      return { label: 'Tomorrow', color: 'success', variant: 'outlined' }
+    else if (dayDifference === -1)
+      return { label: 'Yesterday', color: 'error', variant: 'outlined' }
+    else if (dayDifference < -1)
+      return {
+        label: `${Math.abs(dayDifference)} days ago!`,
+        color: 'error',
+        variant: 'filled',
+      }
+    else return { label: `In ${dayDifference} days`, color: 'success', variant: 'outlined' }
+  }
+
   if (isLoading) {
     return (
       <Card sx={{ margin: '0 1rem' }}>
@@ -64,13 +87,19 @@ export const TodoListContent = ({ activeList, setListCompletion }) => {
         >
           {todos &&
             todos.map((todo, index) => {
+              const dueDateDifference = !todo.completed ? getDueDateDifference(todo.dueDate) : null
               return (
                 <div
                   key={index}
-                  style={{ display: 'flex', alignItems: 'center', padding: '0.5rem' }}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    padding: '0.25rem',
+                    gap: '0.5rem',
+                  }}
                 >
                   <Checkbox
-                    sx={{ margin: '8px' }}
+                    sx={{ flex: '0 0 auto' }}
                     checked={todo.completed}
                     icon={<RadioButtonUncheckedIcon />}
                     checkedIcon={<CheckCircleIcon />}
@@ -84,7 +113,7 @@ export const TodoListContent = ({ activeList, setListCompletion }) => {
                     }}
                   />
                   <TextField
-                    sx={{ flexGrow: 1 }}
+                    sx={{ flex: '6 0 0' }}
                     label='What to do?'
                     value={todo.text}
                     onChange={(event) => {
@@ -96,11 +125,48 @@ export const TodoListContent = ({ activeList, setListCompletion }) => {
                       ])
                     }}
                   />
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      sx={{ flex: '4 0 0' }}
+                      label='Due when?'
+                      format='YYYY-MM-DD'
+                      value={todo.dueDate ? dayjs(todo.dueDate) : null}
+                      onChange={(newDate) => {
+                        setTodos([
+                          ...todos.slice(0, index),
+                          { ...todo, dueDate: newDate ? newDate.format('YYYY-MM-DD') : null },
+                          ...todos.slice(index + 1),
+                        ])
+                      }}
+                      slotProps={{
+                        textField: {
+                          size: 'medium',
+                          clearable: true,
+                        },
+                      }}
+                    />
+                  </LocalizationProvider>
+                  <div
+                    style={{
+                      flex: '1 0 0',
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <Chip
+                      label={dueDateDifference?.label}
+                      color={dueDateDifference?.color}
+                      variant={dueDateDifference?.variant}
+                      size='small'
+                      sx={{
+                        visibility: dueDateDifference ? 'visible' : 'hidden',
+                      }}
+                    />
+                  </div>
                   <Button
-                    sx={{ margin: '8px' }}
+                    sx={{ flex: '0 0 auto' }}
                     size='small'
                     color='secondary'
-                    aria-label={`Delete todo item ${index + 1}`}
                     onClick={() => {
                       setTodos([
                         // immutable delete
